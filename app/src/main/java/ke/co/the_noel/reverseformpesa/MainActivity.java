481a2bb4be1507d487c2418566c897d61f82a2e6
noelomo <heisnoel@gmail.com>
2017-10-30 22:21:35 +0300

    SMS Broadcast receiver

--- app/src/main/java/ke/co/the_noel/reverseformpesa/MainActivity.java
+++ app/src/main/java/ke/co/the_noel/reverseformpesa/MainActivity.java
@@ -2,12 +2,31 @@ package ke.co.the_noel.reverseformpesa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity inst;
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void handleMessage(String smsBody, String address) {
+        Toast.makeText(inst, "Message " + smsBody, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
---
+++ app/src/main/java/ke/co/the_noel/reverseformpesa/smsreceiver/SMSBroadcastReceiver.java
@@ -0,0 +1,37 @@
+package ke.co.the_noel.reverseformpesa.smsreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import ke.co.the_noel.reverseformpesa.MainActivity;
+
+public class SMSBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String SMS_BUNDLE = "pdus";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle intentExtras = intent.getExtras();
+        if (intentExtras != null) {
+            Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
+            String smsMessageStr = "";
+            String smsBody = "", address = "";
+            for (int i = 0; i < sms.length; ++i) {
+                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+
+                smsBody = smsMessage.getMessageBody().toString();
+                address = smsMessage.getOriginatingAddress();
+
+            }
+            Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
+
+            //this will update the UI with message
+            MainActivity inst = MainActivity.instance();
+            inst.handleMessage(smsBody, address);
+        }
+    }
+}
