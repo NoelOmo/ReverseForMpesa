f33c3e9ebb8659efaae5c56182a9de0b489aed0e
noelomo <heisnoel@gmail.com>
2017-11-12 14:59:05 +0300

    Added a method for encrypting initiator password

--- app/src/main/java/ke/co/the_noel/reverseformpesa/MainActivity.java
+++ app/src/main/java/ke/co/the_noel/reverseformpesa/MainActivity.java
@@ -8,10 +8,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
-import android.widget.Toast;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 import ke.co.the_noel.reverseformpesa.API.API;
 import ke.co.the_noel.reverseformpesa.API.APIContract;
@@ -142,6 +158,46 @@ public class MainActivity extends AppCompatActivity {
         return auth;
     }
 
+
+    // Function to encrypt the initiator credentials
+    public static String encryptInitiatorPassword(String securityCertificate, String password) {
+        String encryptedPassword = "YOUR_INITIATOR_PASSWORD";
+        try {
+            Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+            byte[] input = password.getBytes();
+
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding", "BC");
+            FileInputStream fin = new FileInputStream(new File(securityCertificate));
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate) cf.generateCertificate(fin);
+            PublicKey pk = certificate.getPublicKey();
+            cipher.init(Cipher.ENCRYPT_MODE, pk);
+
+            byte[] cipherText = cipher.doFinal(input);
+
+            // Convert the resulting encrypted byte array into a string using base64 encoding
+            encryptedPassword = Base64.encodeToString(cipherText, Base64.NO_WRAP);
+
+        } catch (NoSuchAlgorithmException ex) {
+            //Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchProviderException ex) {
+            //Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchPaddingException ex) {
+            //Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
+            //Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (CertificateException ex) {
+            //Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeyException ex) {
+            //Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalBlockSizeException ex) {
+            //Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (BadPaddingException ex) {
+            //Logger.getLogger(PasswordUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return encryptedPassword;
+    }
 
 
 }
